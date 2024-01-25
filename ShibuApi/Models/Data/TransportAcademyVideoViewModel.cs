42ec37c0891fc28de46dd917b8c7a670d0117cbc
MehmetKUTLUK <120774432+MehmetKUTLUK@users.noreply.github.com>
2024-01-25 10:17:44 +0300

    Add files via upload

---
+++ ShibuApi/Models/Data/TransportAcademyVideoViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ShipbuApi.Models.Data;
+
+public class TransportAcademyVideoViewModel
+{
+    public bool Enabled { get; set; }
+    public string NameTr { get; set; }
+    public string NameEn { get; set; }
+    public string DescriptionTr { get; set; }
+    public string DescriptionEn { get; set; }
+    public string Url { get; set; }
+    public string ImageUrl { get; set; }
+    public int DisplayOrder { get; set; }
+}
---
+++ ShipbuData/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace ShipbuData;
+
+public class AppDbContext : IdentityDbContext<User, Role, Guid>
+{
+    public AppDbContext(DbContextOptions options)
+        : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+
+    public DbSet<TransportOrder> TransportOrders { get; set; }
+    public DbSet<TransportOrderItem> TransportOrderItems { get; set; }
+    public DbSet<TransportOrderItemContainerType> TransportOrderItemContainerTypes { get; set; }
+    public DbSet<TransportOrderItemFeature> TransportOrderItemFeatures { get; set; }
+    public DbSet<TransportStaticPage> TransportStaticPages { get; set; }
+    public DbSet<TransportAcademyVideo> TransportAcademyVideos { get; set; }
+    public DbSet<TransportRegion> TransportRegions { get; set; }
+    public DbSet<TransportDistrict> TransportDistricts { get; set; }
+    public DbSet<TransportMethod> TransportMethods { get; set; }
+    public DbSet<TransportFee> TransportFees { get; set; }
+
+}
